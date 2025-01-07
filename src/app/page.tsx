2c02f480badbe8e07791ed0ac2980bcd1e7910ee
irazramin <iraz.ramin@sjinnovation.com>
2025-01-07 15:19:07 +0600

    folder structure changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import { PAGE_BASE_QUERY_WITH_SLUG } from '@/lib/constant';
 import { getPageBaseData } from '../lib/contentful';
 import Hero from '@/components/Hero';
 
 const Home = async ({ params }: { params: { slug: string } }) => {
-  const page = await getPageBaseData(params.slug);
+  const page = await getPageBaseData(PAGE_BASE_QUERY_WITH_SLUG(params.slug));
 
   return (
     <div className='w-full'>
---
+++ src/utils/shorthandFunctions.ts
@@ -0,0 +1,9 @@
+// export const convertHeroData = (heroData: any) => {
+//     // page.fields.genericPage.fields.heroBanner.fields
+
+//     return {
+//         title: heroData.title,
+//         description: heroData.description,
+//         image: heroData.image,
+//     }
+// }
